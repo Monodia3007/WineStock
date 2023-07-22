d7c89ad7f2732aa41ce27a0fb801eae61d2af336
Pierre Camplin <pierre.camplin.etu@univ-lille.fr>
2023-07-22 09:48:26 +0200

    Created javadoc branch

--- src/main/java/eu/lilithmonodia/winestock/exceptions/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * The package 'eu.lilithmonodia.winestock.exceptions' contains exception classes
- * that define the custom exceptions, which can be thrown during the execution of the WineStock application.
- */
-package eu.lilithmonodia.winestock.exceptions;
--- src/main/java/eu/lilithmonodia/winestock/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * The main package 'eu.lilithmonodia.winestock' is the root package of the WineStock application.
- * <p>
- * It mainly contains the application entry point classes.
- */
-package eu.lilithmonodia.winestock;
