b53e4ab3a3ebbac952367fad25b9b3b42c84d6b8
Pierre Camplin <pierre.camplin.etu@univ-lille.fr>
2023-08-02 00:25:24 +0200

    Update exception package documentation
    Expanded the documentation for the 'eu.lilithmonodia.winestock.exceptions' package to include more detailed descriptions of the custom exceptions present within the WineStock application. This will provide better understanding for the users and developers interacting with the codebase.

--- src/main/java/eu/lilithmonodia/winestock/exceptions/package-info.java
+++ src/main/java/eu/lilithmonodia/winestock/exceptions/package-info.java
@@ -1,5 +1,19 @@
 /**
- * The package 'eu.lilithmonodia.winestock.exceptions' contains exception classes
- * that define the custom exceptions, which can be thrown during the execution of the WineStock application.
+ * The package 'eu.lilithmonodia.winestock.exceptions' includes custom exceptions
+ * tailored to handle issues that may arise during the usage of the WineStock application.
+ * <p>
+ * It includes exceptions such as:
+ * <ul>
+ *     <li><b>InvalidYearException:</b> Thrown when the year provided for a wine is invalid.
+ *     (E.g., year is in the future or far in the past)</li>
+ *     <li><b>InvalidBottleVolumeException:</b> Thrown if the bottle size volume does not
+ *     match any volume in the BottleSize enum.</li>
+ *     <li><b>InvalidWineColorException:</b> Thrown if the set wine color does not align
+ *     with any color in the Color enum.</li>
+ *     <li><b>WineAlreadyInAssortmentException:</b> Thrown to prevent a wine already present
+ *     in an assortment from being added again.</li>
+ *     <li><b>WineNotInAssortmentException:</b> Thrown if an attempt is made to remove a wine
+ *     from an assortment, but the wine is not present in the assortment.</li>
+ * </ul>
  */
 package eu.lilithmonodia.winestock.exceptions;
